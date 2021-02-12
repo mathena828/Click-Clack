d4171f6e7b7e48ad6b180bda1d60a12d2d74cede
chinadupaya <annysiad@yahoo.com>
2021-02-12 18:53:51 +0800

    Create invite code

--- client/src/screens/chat/MessagesPanel.js
+++ client/src/screens/chat/MessagesPanel.js
@@ -1,14 +1,32 @@
-import {useState} from "react";
+import { useState } from "react";
 import Message from './Message';
+import { useCookies } from 'react-cookie';
+import { Button, Container, Row, Col } from "react-bootstrap";
+
 const MessagesPanel = ({onSendMessage, messages, channel}) => {
+    const [cookies, setCookie] = useCookies(['user']);
+    const [copy, setCopy] = useState('Get invite code');
     const [input,setInput] = useState('');
+    function copyToClipboard(){
+        navigator.clipboard.writeText(channel.id);
+        setCopy('Copied!');
+        setTimeout(function(){
+            setCopy('Get invite code');
+        }, 2000); 
+    }
     let list = <div>There are no messages shown</div>;
     if (messages) {
         list = messages.map(m => <Message key={m._id} id={m._id} senderName={m.userName} text={m.content} />);
     }
+    let getInvite = '';
     if(!channel){
         channel = {id:0,name:'Select a channel to discuss!'}
+    }else{
+        if(cookies.user.isTeacher){
+            getInvite = <Button onClick={copyToClipboard}>{copy}</Button>
+        }
     }
+    
     //console.log(props);
     const handleInput=(e)=>{
         setInput(e.target.value);
@@ -22,7 +40,12 @@ const MessagesPanel = ({onSendMessage, messages, channel}) => {
     }
     return(
         <div className="messages-panel">
-            <h3>{channel.name}</h3>
+            <Container style={{marginTop:'1em'}}>
+                <Row>
+                    <Col><h3>{channel.name}</h3></Col>
+                    <Col>{getInvite}</Col>
+                </Row>
+            </Container>
             <div className="messages-list">{list}</div>
             <div className="messages-input"></div>
             <input type="text" onChange={handleInput} value={input}/>‍
