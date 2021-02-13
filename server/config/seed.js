e37c1cd2eaef9c496a17fdd4b050fe29eec210c1
mathena828 <mathena828@gmail.com>
2021-02-13 16:22:57 +0800

    Fixed Seed File

--- server/config/seed.js
+++ server/config/seed.js
@@ -2,7 +2,7 @@ var seeder = require('mongoose-seed');
 
 
 const seedDB = () => {
-    seeder.connect('mongodb://localhost:27017/hello_world', function () {
+    seeder.connect('mongodb://localhost:27017/clickclack', function () {
 
         // Load Mongoose models
         seeder.loadModels([
@@ -12,7 +12,7 @@ const seedDB = () => {
 
         // Clear specified collections
         seeder.clearModels(['User', 'Channel'], function () {
-
+            console.log(data);
             // Callback to populate DB once collections have been cleared
             seeder.populateModels(data, function () {
                 seeder.disconnect();
@@ -20,7 +20,7 @@ const seedDB = () => {
 
         });
     });
-    console.log("Seeded database");
+    console.log("Seeded Database");
 }
 var data = [
     {
@@ -30,27 +30,40 @@ var data = [
                 'username': 'raepineda',
                 'email': 'raepineda@cvmig.edu.ph',
                 'password': "raepineda",
-                'isTeacher': false
+                'isTeacher': false,
+                'bio': 'I am a student from the University of the Philippines. I like to read and travel. I love talking about zoology and philosophy.',
+                'school': 'University of the Philippines',
+                'country': 'Philippines',
             },
             {
                 'username': 'franciscab',
                 'email': 'franciscab@acl.edu.ph',
                 'password': "franciscab",
-                'isTeacher': false
+                'isTeacher': false,
+                'bio': 'I am a student from the University of the Philippines. I enjoy public speaking and I am very passionate about climate change.',
+                'school': 'University of the Philippines',
+                'country': 'Philippines',
             },
             {
                 'username': 'teacher_chi',
                 'email': 'teacherchi@acl.edu.ph',
                 'password': "teacher_chi",
-                'isTeacher': true
+                'isTeacher': true,
+                'bio': 'I am an Algorithm and Complexities Laboratory professor from the University of the Philippines.',
+                'school': 'University of the Philippines',
+                'country': 'Philippines',
             },
             {
                 'username': 'teacher_mathena',
                 'email': 'teachermathena@cvmig.edu.ph',
                 'password': "teacher_mathena",
-                'isTeacher': true
-            },
+                'isTeacher': true,
+                'bio': 'I am a Computer Vision and Machine Intelligence professor from the University of the Philippines.',
+                'school': 'University of the Philippines',
+                'country': 'Philippines',
+             },
         ]
     }
 ];
+seedDB()
 module.exports = seedDB;
